34b0e29b45e81ca101c25f01caf3187fe4a03d9d
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-05-19 01:54:41 -0700

    correction

--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -29,28 +29,35 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.route('/2-hbnb/')
-def hbnb_filters(the_id=None):
-    """
-    handles request to custom template with states, cities & amentities
-    """
-    state_objs = storage.all('State').values()
-    states = dict([state.name, state] for state in state_objs)
-    amens = storage.all('Amenity').values()
-    places = storage.all('Place').values()
+@app.route('/2-hbnb', strict_slashes=False)
+def hbnb():
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
     cache_id = (str(uuid.uuid4()))
+
     return render_template('2-hbnb.html',
-                           states=states,
-                           amens=amens,
+                           states=st_ct,
+                           amenities=amenities,
                            places=places,
                            users=users,
                            cache_id=cache_id)
 
 
 if __name__ == "__main__":
-    """
-    MAIN Flask App"""
+    """ Main Function """
     app.run(host=host, port=port)
 
