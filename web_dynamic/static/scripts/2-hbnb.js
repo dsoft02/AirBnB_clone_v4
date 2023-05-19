a2c4176281281af0d85a131859ac5a60a04c33b8
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-05-19 03:29:15 -0700

    corrections

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -12,9 +12,12 @@ $('document').ready(() => {
     });
 
   $.get('http://100.26.222.79/api/v1/status/', (data) => {
+    console.log(data.status);
     if (data.status === 'OK') {
+      console.log("YES");
       $('div#api_status').addClass('available');
     } else {
+      console.log("NO");
       $('div#api_status').removeClass('available');
     }
   });
